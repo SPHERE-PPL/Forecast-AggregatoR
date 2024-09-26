aa5ec52436a9809db3e4c88c13ba8bb75daa8acc
Dr Josh Tyler <55984880+JTyler13@users.noreply.github.com>
2024-09-26 21:50:07 +0100

    Update Layout

--- foRkast_app/app.R
+++ foRkast_app/app.R
@@ -4,26 +4,47 @@ library(shinyFiles)
 library(gh)
 library(stringr)
 
+
+owner_choices<-c("TuringPPL")
+contest_choices<-c("Competition-Example-Mpox")
+
+
+
 # UI code block
 ui<-fluidPage(
+  theme = shinytheme("flatly"),
+  
   titlePanel("FoRkast"),
   sidebarLayout(
+    
     sidebarPanel(
-      selectInput("original_owner", "Original Owner", choices = "TuringPPL"),
-      selectInput("repo", "Repo", choices = "Competition-Example-Mpox"),
-      textInput("target_file", "Target File", value = "forecast.csv"),
+      selectInput("contest_owner", "Owner", choices = owner_choices),
+      selectInput("repo", "Repo", choices = contest_choices),
+      
+      textInput("target_file", "Target File To Check", value = "forecast.csv"),
       textInput("destination_folder", "Destination Folder", value = paste0(getwd(),"/Contest_Entries")),
-      actionButton("run_button", "Run FoRkast Finder"),
-      h3(textOutput("status"))
+      
+      actionButton(inputId = "run_button", label = "Run FoRkast",icon = icon("play"),class = "btn-lg btn-success"),
+      tags$br(),
+      textOutput("status"),
+      
+      
+      align = "center",
+      tags$style(type="text/css", "#target_file { text-align:center;}"),
+      tags$style(type="text/css", "#destination_folder { text-align:center;}"),
+      tags$style(type = "text/css", "#status { color: #18bc9c; font-size: 18px; font-weight: bold;}") 
     ),
+    
     mainPanel(
       h3("Welcome to FoRkast"),
       p("FoRkast is a tool that finds all forked repos taking part in forecasting
         competitions and checks each submission before cloning the repos locally."),
     )
-  ),
-  theme = shinytheme("flatly")
+    
+  )
 )
+
+
 
 
 # Define the server logic for the Shiny app
@@ -34,13 +55,13 @@ server <- function(input, output,session) {
   observeEvent(input$run_button, {
     
     # Get the input values
-    original_owner <- input$original_owner
+    contest_owner <- input$contest_owner
     repo <- input$repo
     target_file <- input$target_file
     destination_folder <- input$destination_folder
     
     # Get the list of forks
-    forks_data <- as.list(gh("GET /repos/:owner/:repo/forks", owner = original_owner, repo = repo))
+    forks_data <- as.list(gh("GET /repos/:owner/:repo/forks", owner = contest_owner, repo = repo))
     
     # Extract the clone URLs from the API response
     fork_clone_urls <- lapply(forks_data, function(x) x$clone_url)
@@ -56,21 +77,22 @@ server <- function(input, output,session) {
     if (!dir.exists(errors_folder)) {
       dir.create(errors_folder)
     }
+    
     ################################################################################### 
     # RUN THROUGH CHECKS AND CLONES FOR EACH REPO  ####################################
     ###################################################################################
     
     for (fork_url in fork_clone_urls) {
       
-      owner <- strsplit(fork_url, "/")[[1]][4]
-      repo_name <- str_extract(fork_url, "[^/]+\\.git$")
-      repo_name <- str_remove(repo_name, "\\.git$")
+      forked_owner <- strsplit(fork_url, "/")[[1]][4]
+      forked_repo_name <- str_extract(fork_url, "[^/]+\\.git$")
+      forked_repo_name <- str_remove(forked_repo_name, "\\.git$")
       
       # Create Error List
       errors <- list()
       
       # Find the contents of the submissions folder in the forked repository
-      repo_contents <- gh("GET /repos/:owner/:repo/contents/submission", owner = owner, repo = repo_name)
+      repo_contents <- gh("GET /repos/:owner/:repo/contents/submission", owner = forked_owner, repo = forked_repo_name)
       
       # Find the target file in the repository contents & if not there, add to errors
       file_info <- repo_contents[sapply(repo_contents, function(x) x$name == target_file)]
@@ -79,12 +101,52 @@ server <- function(input, output,session) {
         errors <- c(errors, "Forecast file not found")
         
       } else {
+        
+        
         ################################################################################### 
-        # CHECK THE FORECAST FOR ERRORS USING SUBMISSION TESTS#############################
+        # CHECK THE FORECAST FOR ERRORS USING SUBMISSION TESTS ############################
         ###################################################################################
         
         if(repo=="Competition-Example-Mpox"){
-          source(file = "submission_tests/mpox_tests.R", local = TRUE)
+          # Fetch the file content (assuming it's a text file)
+          file_content_encoded <- gh("GET /repos/:owner/:repo/contents/submission/:path",
+                                     owner = forked_owner, repo = forked_repo_name, path = target_file)$content
+          
+          # Decode the content from base64
+          file_content <- rawToChar(base64enc::base64decode(file_content_encoded))
+          
+          # Split the text into lines
+          lines <- strsplit(file_content, "\n")[[1]]
+          
+          # Extract column names from the first line
+          col_names <- strsplit(lines[1], ",")[[1]]
+          
+          # Split each remaining line into columns and create a list of rows
+          data_rows <- lapply(lines[-1], function(line) strsplit(line, ",")[[1]])
+          
+          # Create the data.frame
+          df <- as.data.frame(do.call(rbind, data_rows), stringsAsFactors = FALSE)
+          colnames(df) <- col_names
+          rm(lines, col_names, data_rows,file_content_encoded,file_content)
+          
+          # Check if the data.frame has the correct columns
+          if(!all(c("who_region", "month_start","Estimate") %in% colnames(df))){
+            errors <- c(errors, "Incorrect column names")
+          }
+          
+          # Check if the forecast column is numeric
+          if(!all(str_detect(df$Estimate, "^-?\\d+(\\.\\d+)?$"))){
+            errors <- c(errors, "Forecast column is not numeric")
+          }
+          
+          # Check if the forecast column contains -9999
+          if(-9999 %in% df$Estimate){
+            errors <- c(errors, "Forecast column contains -9999")
+          } 
+          
+          
+        }else{
+          errors <- c(errors, "No tests available for this competition")
         }
         
       }
@@ -97,7 +159,7 @@ server <- function(input, output,session) {
       if(length(errors)==0){ # Check if there are no errors
         
         # Construct the full path for cloning
-        clone_path <- file.path(destination_folder, paste0(owner,"_",repo_name))
+        clone_path <- file.path(destination_folder, paste0(forked_owner,"_",forked_repo_name))
         
         # Clone the repository using `sprintf` for clearer formatting
         system_command <- sprintf("git clone %s %s", fork_url, paste0("'",clone_path,"'"))
@@ -105,7 +167,7 @@ server <- function(input, output,session) {
         
       }else{ # If there are errors
         # Construct the full path for cloning
-        clone_path <- file.path(errors_folder, paste0(owner,"_",repo_name))
+        clone_path <- file.path(errors_folder, paste0(forked_owner,"_",forked_repo_name))
         
         if (!dir.exists(clone_path)) {
           dir.create(clone_path)
@@ -122,7 +184,7 @@ server <- function(input, output,session) {
       }
     }
     
-    output$status <- renderText("FoRkast Finder Complete")
+    output$status <- renderText("FoRkast Complete - See Errors Folder for Repos with Issues")
     
   })
 }
