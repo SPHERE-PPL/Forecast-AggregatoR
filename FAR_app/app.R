b1d482356b19600eb55d33b69b1c9c4fab7326e3
Dr Josh Tyler <55984880+JTyler13@users.noreply.github.com>
2025-02-11 15:47:57 +0000

    Add HFC-Lyme-Disease (initial)

--- FAR_app/app.R
+++ FAR_app/app.R
@@ -15,7 +15,7 @@ sidebarPanel2 <- function (..., out = NULL, width = sidebar_width) {
 
 
 owner_choices<-c("SPHERE-PPL")
-contest_choices<-c("Competition-Example-Mpox")
+contest_choices<-c("Competition-Example-Mpox","HFC-Lyme-Disease")
 
 
 
@@ -33,7 +33,7 @@ ui<-fluidPage(
     sidebarPanel2(
 
       radioButtons("contest_owner", "Owner", choices = owner_choices,inline = TRUE),
-      radioButtons("repo", "Repo", choices = contest_choices,inline = TRUE),
+      radioButtons("repo", "Repo", choices = contest_choices,inline = FALSE),
       
       textInput("target_file", "Target File To Check", value = "forecast.csv"),
       textInput("destination_folder", "Destination Folder", value = paste0(getwd(),"/Contest_Entries")),
@@ -174,6 +174,40 @@ server <- function(input, output,session) {
           if(-9999 %in% df$Estimate){
             errors <- c(errors, "Forecast column contains -9999")
           } 
+          
+          
+        }else{
+          errors <- c(errors, "No tests available for this competition")
+        }
+        
+        
+        if(repo=="HFC-Lyme-Disease"){
+          # Fetch the file content (assuming it's a text file)
+          file_content_encoded <- gh("GET /repos/:owner/:repo/contents/submission/:path",
+                                     owner = forked_owner, repo = forked_repo_name, path = target_file)$content
+          
+          # Decode the content from base64
+          file_content <- rawToChar(base64enc::base64decode(file_content_encoded))
+          
+          # Split the text into lines
+          lines <- strsplit(file_content, "\n")[[1]]
+          lines <- str_remove(lines, "\r")
+          # Extract column names from the first line
+          col_names <- strsplit(lines[1], ",")[[1]]
+          
+          # Split each remaining line into columns and create a list of rows
+          data_rows <- lapply(lines[-1], function(line) strsplit(line, ",")[[1]])
+          
+          # Create the data.frame
+          df <- as.data.frame(do.call(rbind, data_rows), stringsAsFactors = FALSE)
+          colnames(df) <- col_names
+          rm(lines, col_names, data_rows,file_content_encoded,file_content)
+          
+          # Check if the data.frame has the correct columns
+          if(!all(c("Year","Council","Case","Population","Incidence","Lower_95CI","Upper_95CI") %in% colnames(df))){
+            errors <- c(errors, "Incorrect column names")
+          }
+          
           
           
         }else{
